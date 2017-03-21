42016223bf3723998afd2b6162a770a9a224428b
Omar Parkes <omr.prks@gmail.com>
2017-03-21 17:12:33 +0800

    Updated file structure

--- lib/colourize.rb
+++ lib/colourize.rb
@@ -1,12 +1,12 @@
 class String
 	public
-	def red() colourize(31) end
-	def green() colourize(32) end
-	def yellow() colourize(33) end
-	def blue() colourize(34) end
-	def lightblue() colourize(36) end
-	def pink() colourize(35) end
+	def red() colours(31) end
+	def green() colours(32) end
+	def yellow() colours(33) end
+	def blue() colours(34) end
+	def lightblue() colours(36) end
+	def pink() colours(35) end
 
 	private
-	def colourize(_) "\e[#{_}m#{self}\e[0m" end
+	def colours(_) "\e[#{_}m#{self}\e[0m" end
 end
--- lib/gitignore.rb
+++ lib/gitignore.rb
@@ -3,9 +3,12 @@ class Gitignore
 
 	public
 	def self.list
-		Dir[File.dirname(__FILE__) + "/**/*.gitignore"].each do
-			|_| puts File.basename(_, File.extname(_))
-		end
+		$TEMPLATES = Dir["#{File.dirname(__FILE__)}/**/*.gitignore"]
+		# (0..$TEMPLATES.length - 1).step(2).each do |_|
+		# 	puts File.basename($TEMPLATES[_], File.extname($TEMPLATES[_])) + " " +
+		# 		File.basename($TEMPLATES[_ + 1], File.extname($TEMPLATES[_ + 1]))
+		# end
+		$TEMPLATES.each { |_| puts File.basename(_, File.extname(_)) }
 	end
 
 	def self.show(_) puts File.open(_, 'r').read if File.exist?(_) end
