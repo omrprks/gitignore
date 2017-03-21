ea7d151a8a017b6ce788a0d52ce509c42dff0cd1
Omar Parkes <omr.prks@gmail.com>
2017-03-21 16:45:52 +0800

    Updated file structure

--- lib/gitignore.rb
+++ lib/gitignore.rb
@@ -1,11 +1,13 @@
 class Gitignore
-	@@url = "https://www.gitignore.io/api/"
+	@@URL = "https://www.gitignore.io/api/"
 
 	public
 	def self.list
-		Dir[File.dirname(__FILE__) + "/**/*.gitignore"].each { |_| puts File.basename(_) }
+		Dir[File.dirname(__FILE__) + "/**/*.gitignore"].each do
+			|_| puts File.basename(_, File.extname(_))
+		end
 	end
 
 	def self.show(_) puts File.open(_, 'r').read if File.exist?(_) end
-	def self.url() @@url end
+	def self.url() @@URL end
 end
